0067a16be34e7039563df6cfc6773a4b7c3f376d
muuto <morikawa-muuto@MacBook-Air-of-Muuto.local>
2014-07-13 10:43:05 +0900

    first  commit

---
+++ どのフレーズ使うッター/Serihu.m
@@ -0,0 +1,13 @@
+//
+//  Serihu.m
+//  どのフレーズ使うッター
+//
+//  Created by 森川夢佑斗 on 2014/07/13.
+//  Copyright (c) 2014年 森川夢佑斗. All rights reserved.
+//
+
+#import "Serihu.h"
+
+@implementation Serihu
+
+@end
---
+++ どのフレーズ使うッター/SuportViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuportViewController.h
+//  どのフレーズ使うッター
+//
+//  Created by 森川夢佑斗 on 2014/07/06.
+//  Copyright (c) 2014年 森川夢佑斗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuportViewController : UIViewController
+
+@end
